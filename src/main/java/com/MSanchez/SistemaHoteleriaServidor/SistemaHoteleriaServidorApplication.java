bd025f53323d88a8494426d7a2195a5411b4465e
Pokebruno-32 <brunomoal27@hotmail.com>
2026-04-07 12:34:23 -0600

    Creacion de repositorio + conexion a BD

---
+++ src/main/java/com/MSanchez/SistemaHoteleriaServidor/SistemaHoteleriaServidorApplication.java
@@ -0,0 +1,13 @@
+package com.MSanchez.SistemaHoteleriaServidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaHoteleriaServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaHoteleriaServidorApplication.class, args);
+	}
+
+}
